c65017b11585cd49c83c20a780c96b31112c4ac0
sks9919 <sksshashank901@gmail.com>
2016-10-31 17:30:23 +0530

    Added GCD

---
+++ GCD.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int gcdFunction(int x, int y)
+{
+  while( (x %= y) && (y %= x) );
+  return x + y;
+}
+
+int main()
+{
+	int x, y;
+	cin>>x;
+	cin>>y;
+	cout << "GCD of numbers " << x << " and " << y << " is " << gcdFunction(x, y) << endl;
+
+	return 0;
+}
+
+// Output is GCD of two Given Numbers
